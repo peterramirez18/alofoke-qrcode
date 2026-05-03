3e9684688a464f8d7aa26e340b2063a99fafb4fe
peterramirez18 <peterramirez1812@gmail.com>
2026-05-03 09:56:54 -0400

    ReferenceError: f is not defined

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("Telegram QR Bot funcionando ✅");
-});f
+});
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Servidor escuchando en puerto ${PORT}`);
